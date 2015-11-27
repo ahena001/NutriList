7d380ff0f091e39d4ec66413c519bd79ce3ac3df
Leonardo Martin <lmart296@fiu.edu>
2015-11-27 00:44:28 -0500

    parse framework added, recipe list stubbed. Parse still not implemented.

---
+++ NutriList/RecipeDetailController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeDetailController.h
+//  
+//
+//  Created by Leonardo Martin on 11/26/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeDetailController : UIViewController
+
+@end
---
+++ NutriList/RecipeDetailController.m
@@ -0,0 +1,13 @@
+//
+//  RecipeDetailController.m
+//  
+//
+//  Created by Leonardo Martin on 11/26/15.
+//
+//
+
+#import "RecipeDetailController.h"
+
+@implementation RecipeDetailController
+
+@end
