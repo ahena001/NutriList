24e4a2ff702fd467f139bd22aa29b8471dfb6afe
Leonardo Martin <lmart296@fiu.edu>
2015-12-08 22:41:23 -0500

    added cookbook view controller implementation

--- NutriList/CookbookListController.h
+++ NutriList/CookbookListController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CookbookListController : UITableViewController
+@interface CookbookListController : UITableViewController <UINavigationControllerDelegate>
 
 
 @end
--- NutriList/CookbookListController.m
+++ NutriList/CookbookListController.m
@@ -24,4 +24,31 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of entries in the shared addressbook .
+    //    return [myAddressBook.myAddressBook count];
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"cookbook";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if(cell == nil){
+        cell = ([[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier]);
+    }
+    
+    //and makes a cell out of it
+    cell.textLabel.text = @"lol";
+    
+    return cell;
+}
+
+
 @end
